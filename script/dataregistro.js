bd98a37c1ec1555b5d5c5a30eebf7ea0e2265848
vanessa rodriguez <vanero0811@gmail.com>
2022-01-11 13:52:39 -0500

    cambios

--- script/dataregistro.js
+++
@@ -1,9 +0,0 @@
-export const registro = [ {
-
-    "nombre": 'Nombres:',
-    "apellidos": 'Apellidos:',
-    "telefono": 'Telefono:',
-    "direccion": 'Direccion:',
-    "observaciones": 'Observaciones:',
-
-}]
--- script/registro.js
+++ script/registro.js
@@ -1,28 +1,47 @@
-import {registro} from "./dataregistro.js";
+let datos=JSON.parse(localStorage.getItem('usuario')) || []
 
-let mostrar=document.getElementById('mostrar')
+const formulario = document.getElementById("formulario")
 
-mostrar.innerHTML=''
+const listar = document.getElementById("listar")
 
-registro.forEach(regis => {
-    const{nombre,apellidos,telefono,direccion,observaciones}= regis 
-    mostrar.innerHTML+= `<div>
-    <form action=""> <b>
-    <label for="nombre">${nombre}</label>
-    <input type="text" name="nombre" placeholder="Escribe tus nombres"> <br>
-    <label for="apellido">${apellidos}</label>
-    <input type="text" name="apellido" placeholder="Escribe tus apellidos" id="apellido"> <br>
-    <label for="telefono">${telefono}</label>
-    <input type="text" name="telefono" placeholder="Escribe tu telefono" id="telefono"><br>
-    <label for="direccion">${direccion}</label>
-    <input type="text" name="direccion" placeholder="Escribe tu direccion" id="direccion"><br>
-    <br>
-    <label for="observaciones">${observaciones}</label> <br>
-    <br>
-    <textarea name="" id="" cols="30" rows="10"></textarea>
-    <br>
-    </div>
-              
-    `
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let Nombres = document.getElementById("Nombres").value;
+    let Apellidos = document.getElementById("Apellidos").value;
+    let Telefono = document.getElementById("Telefono").value;
+    let Direccion = document.getElementById("Direccion").value;
+    let Observaciones = document.getElementById("Observaciones").value;
+
+
+let registro = {
+    Nombres :Nombres,
+    Apellidos : Apellidos,
+    Telefono: Telefono,
+    Direccion: Direccion,
+    Observaciones: Observaciones   
+}
+
+datos.push(registro)
+console.log(datos)
+localStorage.setItem("usuario",JSON.stringify(datos))
+     listarDatos();
+
 })
+    const listarDatos = () => {
+    listar.innerHTML=""
+    datos = JSON.parse(localStorage.getItem("usuario"))
+    datos.forEach(element => {
+        const {Nombres,Apellidos,Telefono,Direccion,Observaciones} = element
+        listar.innerHTML+= `<div class="card" style="width: 18rem;">
+        <div class="card.body">
+          <h5 class="card-title">${Nombres}</h5>
+          <h6 class="card-subtitle mc-2 text-muted">${Apellidos}</h6>
+          <h6 class="card-subtitle mc-2 text-muted">${Telefono}</h6>
+          <h6 class="card-subtitle mc-2 text-muted">${Direccion}</h6>
+          <h6 class="card-subtitle mc-2 text-muted">${Observaciones}</h6>
+        </div>
+      </div>`
+        
+  });
+}
 
